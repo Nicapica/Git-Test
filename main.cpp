f2376aae2ea66284fc5e353e594a57c03d754fcf
Nicapica <nicapica@ut.utm.edu>
2018-11-27 19:54:46 -0600

    Fifth commit: adding new main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int sum(int n){
+  int answer = 0;
+  for( int i = 0; i <= n; i++){
+    answer += i;
+  }
+  return answer;
+}
+
 int main() {
-  cout << "Hello World!" << endl;
+  int n = 0;
+  cout << "please input a number" << endl;
+  cin >> n;
+  cout << sum(n) << endl;
   return 0;
 }
