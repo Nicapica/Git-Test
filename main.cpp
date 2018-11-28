f368d090be9412340dd36a73e5e9b71965ec0fd5
Nicapica <nicapica@ut.utm.edu>
2018-11-27 21:15:43 -0600

    new main.cpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,22 @@ int sum(int n){
   return answer;
 }
 
+int product(int p){
+  int product_answer = 0;
+  for (int i = 1; i <= p; i++){
+    product_answer *= i;
+  }
+  return product_answer;
+}
+
 int main() {
   int n = 0;
+  int p = 0;
   cout << "please input a number" << endl;
   cin >> n;
   cout << sum(n) << endl;
+  cout << "please input a new number" << endl;
+  cin >> p;
+  cout << product(p) << endl;
   return 0;
 }
